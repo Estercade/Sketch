c2e3414b3db36d3b49e0a7fdae9b0fd614b5f412
Estercade <withrwithoutyou@gmail.com>
2023-04-01 11:02:37 -0400

    Add logic for resizing grid
    Add event listener for resizing button
    
    Moved initialization of grid to function
    which is run after document first loads

--- js/javascript.js
+++ js/javascript.js
@@ -3,17 +3,19 @@ const gridContainer = document.getElementById('gridContainer');
 let width = 16;
 let height = 16;
 
-for (let i = 0; i < height; i++) {
-    const tempRow = document.createElement('div');
-    tempRow.setAttribute('id', `row${i}`);
-    for (let j = 0; j < width; j++) {
-        const tempGrid = document.createElement('div');
-        tempGrid.setAttribute('id', `gridBox${i}${j}`);
-        tempGrid.setAttribute('class', 'gridBox');
-        tempGrid.textContent = `${i}${j}`;
-        tempRow.appendChild(tempGrid)
+function generateGrid () {
+    for (let i = 0; i < height; i++) {
+        const tempRow = document.createElement('div');
+        tempRow.setAttribute('id', `row${i}`);
+        for (let j = 0; j < width; j++) {
+            const tempGrid = document.createElement('div');
+            tempGrid.setAttribute('id', `gridBox${i}${j}`);
+            tempGrid.setAttribute('class', 'gridBox');
+            tempGrid.textContent = `${i}${j}`;
+            tempRow.appendChild(tempGrid)
+        }
+        gridContainer.appendChild(tempRow)
     }
-    gridContainer.appendChild(tempRow)
 }
 
 // Add event listeners to gridBoxes for hover function
@@ -24,3 +26,17 @@ gridBoxes.forEach(gridBox =>
 function colorChange(e) {
     e.target.setAttribute('style', 'background-color:black');
 }
+
+const sizeBtn = document.getElementById('sizeBtn');
+sizeBtn.addEventListener('click', resize);
+
+function resize () {
+    height = prompt("How tall would you like the sketchpad to be?");
+    width = prompt("How wide would you like the sketchpad to be?");
+    while (gridContainer.hasChildNodes()){
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    generateGrid();
+}
+
+document.onload = generateGrid();
