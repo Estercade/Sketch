efbb5c86681b77524d70ffa35b85ffe1f81e1c7b
Estercade <withrwithoutyou@gmail.com>
2023-04-01 10:25:14 -0400

    Add event listeners for gridBoxes
    Add logic to give gridBoxes class of gridBox
    for querySelectorAll

--- js/javascript.js
+++ js/javascript.js
@@ -5,9 +5,16 @@ for (let i = 0; i < 16; i++) {
     tempRow.setAttribute(`id`, `row${i}`);
     for (let j = 0; j < 16; j++) {
         const tempGrid = document.createElement('div');
-        tempGrid.setAttribute(`id`, `grid${i}${j}`);
+        tempGrid.setAttribute(`id`, `gridBox${i}${j}`);
+        tempGrid.setAttribute(`class`, `gridBox`);
         tempGrid.textContent = `${i}${j}`;
         tempRow.appendChild(tempGrid)
     }
     gridContainer.appendChild(tempRow)
 }
+
+// Add event listeners to gridBoxes for hover function
+const gridBoxes = document.querySelectorAll(`.gridBox`);
+gridBoxes.forEach(gridBox => 
+    gridBox.addEventListener('mouseover', () => 
+    console.log(`1`)));
